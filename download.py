94b867e34301a517caee34ec8dd39cf5ad64a017
Suli Yang <yangsuli@gmail.com>
2017-01-08 19:01:34 -0600

    add autosending to kindle

--- download.py
+++ download.py
@@ -6,6 +6,14 @@ from BeautifulSoup import BeautifulSoup
 import re
 import gzip
 
+import smtplib
+from email.MIMEMultipart import MIMEMultipart
+from email.MIMEBase import MIMEBase
+from email.MIMEText import MIMEText
+from email import encoders
+
+from subprocess import call
+
 def remove_tags(text):
 	return ''.join( BeautifulSoup(text).findAll( text = True )) 
 
@@ -85,14 +93,55 @@ def get_chapter(chapter_url, cj, out_fp):
 
 			out_fp.write(''.join(c for c in line if u'\u4e00' <= c <= u'u9fff'))
 
+def get_title(novelid):
+	url = "http://www.jjwxc.net/onebook.php?novelid=" + str(novelid)
+	response = requests.get(url)
+	response.encoding='gb18030'
+	for line in response.text.split('\n'):
+		if "articleSection" in line:
+			line = remove_tags(line)
+			return line.strip()
+
+def send_email_attachment(fromaddr, toaddr, password, attach_filepath, attach_filename):
+	msg = MIMEMultipart()
+	msg['From'] = fromaddr
+	msg['To'] = toaddr
+	msg['Subject'] = "book"
+	 
+	body = ""
+	msg.attach(MIMEText(body, 'plain'))
+	
+	attachment = open(attach_filepath, "rb")
+	part = MIMEBase('application', 'octet-stream')
+	part.set_payload((attachment).read())
+	encoders.encode_base64(part)
+	part.add_header('Content-Disposition', "attachment; filename= %s" % attach_filename)
+	msg.attach(part)
+	 
+	server = smtplib.SMTP('smtp.gmail.com', 587)
+	server.starttls()
+	server.login(fromaddr, password)
+	text = msg.as_string()
+	server.sendmail(fromaddr, toaddr, text)
+	server.quit()
+
+
 cj = browsercookie.chrome()
 novel_id_str = raw_input("what is the novelid you want to download?")
 novel_id = int(novel_id_str)
+title = get_title(novel_id)
 urls = get_chapter_urls(novel_id)
-text_file = open("output.txt", "w")
+text_file = open(title + ".txt", "w")
 for url in urls:
 	print "get url:", url
 	get_chapter(url, cj, text_file)
 text_file.close()
 
-call(["/Applications/calibre.app/Contents/console.app/Contents/MacOS/ebook-convert", "output.txt", "output.mobi"])
+kindle_sendaddr = ""
+password = ''
+kindle_recvaddr = ""
+filename = (title + ".txt").encode('utf-8')
+filepath = title + ".txt"
+send_email_attachment(kindle_sendaddr, kindle_recvaddr, password, filepath, filename)
+
+call(["/Applications/calibre.app/Contents/console.app/Contents/MacOS/ebook-convert", title + ".txt", title + ".mobi"])
